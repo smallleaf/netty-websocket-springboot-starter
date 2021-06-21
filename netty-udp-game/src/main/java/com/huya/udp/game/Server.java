5477c7c42908c5ee34ec17bd06e5d22c2334cfce
yesheng <yesheng@huya.com>
2021-06-21 17:56:46 +0800

    尝试netty udp 做游戏服务端

---
+++ netty-udp-game/src/main/java/com/huya/udp/game/Server.java
@@ -0,0 +1,32 @@
+package com.huya.udp.game;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+/**
+ * \* @Author: yesheng
+ * \* Date: 2021/6/21 17:25
+ * \* Description:
+ * \
+ */
+public class Server {
+    private Channel serverChannel;
+
+    public void onStart(int port){
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            ServerHandler serverHandler = new ServerHandler(serverChannel);
+            bootstrap.group(group).channel(NioDatagramChannel.class)
+                    .handler(serverHandler);
+            serverChannel = bootstrap.bind(port).sync().channel();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+}
